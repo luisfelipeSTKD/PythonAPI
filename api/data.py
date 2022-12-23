e1adb744cfff707a226530e330e007d3f3864720
luisfelipeSTKD <lfstorkidias@gmail.com>
2022-12-23 14:23:04 -0300

    adicionado downloader

---
+++ api/data.py
@@ -0,0 +1,39 @@
+import sys 
+sys.path.append('../') # para relative imports
+
+import os
+import requests
+
+import pandas as pd
+
+from IPython.core.interactiveshell import InteractiveShell
+
+
+######## LOAD DATASET ########
+
+def downloader(url: str, dest_folder: str):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+
+    filename = url.split('/')[-1].replace(" ", "_")
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        print("saving to", os.path.abspath(file_path))
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:
+        print(f"Download failed: status code {r.status_code}\n{r.text}")
+
+###### URL #####
+
+url_to_titanic_data = 'https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
+downloader(url_to_titanic_data, './data')
+
+###### TRANSFORM DATA ######
+
